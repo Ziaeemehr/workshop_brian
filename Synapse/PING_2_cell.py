33a134b6d71ab55ff05e851c3d1d46180864795e
Abolfazl Ziaeemehr <a.ziaeemehr@gmail.com>
2020-08-29 13:23:40 +0430

    ping 2 cell added.

--- Synapse/PING_2_cell.py
+++ Synapse/PING_2_cell.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot_data(st_mon_rt, st_mon_wb, title=None, c='k'):
+def plot_data(st_mon_e, st_mon_i, title=None, c='k'):
     """Plots the state_monitor variables "vm" vs. time.
 
     Args:
@@ -13,21 +13,21 @@ def plot_data(st_mon_rt, st_mon_wb, title=None, c='k'):
 
     fig, ax = plt.subplots(2, figsize=(10, 6), sharex=True)
 
-    ax[0].plot(st_mon_rt.t / b2.ms, st_mon_rt.vm[0] /
+    ax[0].plot(st_mon_e.t / b2.ms, st_mon_e.vm_e[0] /
                b2.mV, lw=2, c="r", alpha=0.5, label="neuron RTM")
-    ax[0].plot(st_mon_wb.t / b2.ms, st_mon_wb.vm[0] /
+    ax[0].plot(st_mon_i.t / b2.ms, st_mon_i.vm_i[0] /
                b2.mV, lw=2, c="b", alpha=0.5, label='neuron WB')
 
-    ax[1].plot(st_mon_rt.t / b2.ms, st_mon_rt.s_in[0],
-               lw=2, c="r", label='s_in, source')
-    ax[1].plot(st_mon_wb.t / b2.ms, st_mon_wb.s_in[0],
-               lw=2, c="b", label='s_in, target')
+    ax[1].plot(st_mon_e.t / b2.ms, st_mon_e.s_e[0],
+               lw=2, c="r", label='s_e')
+    ax[1].plot(st_mon_i.t / b2.ms, st_mon_i.s_i[0],
+               lw=2, c="b", label='s_i')
 
-    ax[0].set_xlabel("t [ms]")
-    ax[0].set_ylabel("v [mV]")
-    ax[1].set_ylabel("s")
+    ax[0].set_xlabel("t [ms]", fontsize=14)
+    ax[0].set_ylabel("v [mV]", fontsize=14)
+    ax[1].set_ylabel("s", fontsize=14)
 
-    ax[0].set_xlim(0, np.max(st_mon_rt.t / b2.ms))
+    ax[0].set_xlim(0, np.max(st_mon_e.t / b2.ms))
     ax[0].set_ylim(-100, 50)
     ax[1].set_ylim(0, 1)
     ax[0].legend()
@@ -43,145 +43,169 @@ def plot_data(st_mon_rt, st_mon_wb, title=None, c='k'):
 
 def simulate_2_cell_PING():
 
-    num_rt = 1
-    num_wb = 1
-    I_ext_rt = 1.4 * b2.uA
-    I_ext_wb = 0 * b2.uA
+    num_e = 1
+    num_i = 1
+    I_ext_e = 1.4 * b2.uA
+    I_ext_i = 0 * b2.uA
 
-    gSyn_rt = 0.25 * b2.msiemens
-    gSyn_wb = 0.25 * b2.msiemens
-    weight = 0.25       # connection weight of tr to wb cell.
+    weight_ei = weight_ie = 1  # connection weight of tr to wb cell.
+
+    v0_e = -70 * b2.mV
+    v0_i = -63 * b2.mV
 
     simulation_time = 200.0 * b2.ms
-    integration_method = "euler"
+    integration_method = "rk4"
 
-    # RTM neuron parameters
-    El = -67 * b2.mV
-    EK = -100 * b2.mV
-    ENa = 50 * b2.mV
-    ESyn = 0 * b2.mV
-    gl = 0.1 * b2.msiemens
-    gK = 80 * b2.msiemens
-    gNa = 100 * b2.msiemens
-    C = 1 * b2.ufarad
+    params_e = {'El': -67.0 * b2.mV,
+                'EK': -100.0 * b2.mV,
+                'ENa': 50.0 * b2.mV,
+                'gl': 0.1 * b2.msiemens,
+                'gK': 80.0 * b2.msiemens,
+                'gNa': 100.0 * b2.msiemens,
+                'C': 1.0 * b2.ufarad,
+                'tau_d_e': 3.0 * b2.ms,
+                'tau_r_e': 0.5 * b2.ms,
+                # 'tau_dq_e': 0.17228803581986085 * b2.ms
+                }
 
-    v0_rt = -70 * b2.mV
-    v0_wb = -63 * b2.mV
+    # WB neuron parameters-------------------------------------------
+    params_i = {'El': -65.0 * b2.mV,
+                'EK': -90.0 * b2.mV,
+                'ENa': 55.0 * b2.mV,
+                'gl': 0.1 * b2.msiemens,
+                'gK': 9.0 * b2.msiemens,
+                'gNa': 35.0 * b2.msiemens,
+                'C': 1.0 * b2.ufarad,
+                'tau_d_i': 9.0 * b2.ms,
+                'tau_r_i': 0.5 * b2.ms,
+                # 'tau_dq_i': 0.11628780300799235 * b2.ms,
+                }
 
-    tau_d = 2 * b2.ms
-    tau_r = 0.2 * b2.ms
+    params_syn = {
+        "g_ei": 0.25 * b2.msiemens,
+        "g_ie": 0.25 * b2.msiemens,
+        'v_rev_e': 0 * b2.mV,
+        'v_rev_i': -75 * b2.mV,
+    }
 
+    eqs_e = """
 
-    # forming RTM model with differential equations
-    eqs_rt = """
+    alphah = 0.128 * exp(-(vm_e + 50.0*mV) / (18.0*mV))/ms :Hz
+    alpham = 0.32/mV * (vm_e + 54*mV) / (1.0 - exp(-(vm_e + 54.0*mV) / (4.0*mV)))/ms:Hz
+    alphan = 0.032/mV * (vm_e + 52*mV) / (1.0 - exp(-(vm_e + 52.0*mV) / (5.0*mV)))/ms:Hz
 
-    alphah = 0.128 * exp(-(vm + 50.0*mV) / (18.0*mV))/ms :Hz
-    alpham = 0.32/mV * (vm + 54*mV) / (1.0 - exp(-(vm + 54.0*mV) / (4.0*mV)))/ms:Hz
-    alphan = 0.032/mV * (vm + 52*mV) / (1.0 - exp(-(vm + 52.0*mV) / (5.0*mV)))/ms:Hz
+    betah  = 4.0 / (1.0 + exp(-(vm_e + 27.0*mV) / (5.0*mV)))/ms:Hz
+    betam  = 0.28/mV * (vm_e + 27.0*mV) / (exp((vm_e + 27.0*mV) / (5.0*mV)) - 1.0)/ms:Hz
+    betan  = 0.5 * exp(-(vm_e + 57.0*mV) / (40.0*mV))/ms:Hz
 
-    betah  = 4.0 / (1.0 + exp(-(vm + 27.0*mV) / (5.0*mV)))/ms:Hz
-    betam  = 0.28/mV * (vm + 27.0*mV) / (exp((vm + 27.0*mV) / (5.0*mV)) - 1.0)/ms:Hz
-    betan  = 0.5 * exp(-(vm + 57.0*mV) / (40.0*mV))/ms:Hz
-
-    membrane_Im = I_ext + gNa*m**3*h*(ENa-vm) + \
-        gl*(El-vm) + gK*n**4*(EK-vm) + gSyn*s_in*(-vm): amp
+    membrane_Im = I_ext + gNa*m**3*h*(ENa-vm_e) + \
+        gl*(El-vm_e) + gK*n**4*(EK-vm_e) + I_syn_e: amp
     I_ext : amp
-    s_in  : 1
-    gSyn  : siemens
+    I_syn_e : amp
 
     dm/dt = alpham*(1-m)-betam*m : 1
     dn/dt = alphan*(1-n)-betan*n : 1
     dh/dt = alphah*(1-h)-betah*h : 1
-    
-    ds/dt = 0.5 * (1 + tanh(0.1*vm/mV)) * (1-s)/tau_r - s/tau_d : 1
 
-    dvm/dt = membrane_Im/C : volt
+    ds_e/dt = 0.5 * (1 + tanh(0.1*vm_e/mV)) * (1-s_e)/tau_r_e - s_e/tau_d_e : 1
+    
+    dvm_e/dt = membrane_Im/C : volt
     """
 
-    neuron_rt = b2.NeuronGroup(num_rt, eqs_rt, method="euler",
-                               dt=0.01*b2.ms,
-                               threshold='vm>-55*mV')
+    neuron_e = b2.NeuronGroup(num_e,
+                              eqs_e,
+                              method=integration_method,
+                              dt=0.05*b2.ms,
+                              threshold='vm_e>-55*mV',
+                              refractory='vm_e>-55*mV',
+                              namespace=params_e)
 
     # initialize variables
-    neuron_rt.vm = v0_rt
-    neuron_rt.m = "alpham / (alpham + betam)"
-    neuron_rt.h = "alphah / (alphah + betah)"
-    neuron_rt.n = "alphan / (alphan + betan)"
-    neuron_rt.I_ext = I_ext_rt
-    neuron_rt.s_in = 0
-    neuron_rt.gSyn = gSyn_rt
-
-    # WB neuron parameters
-    El = -65 * b2.mV
-    EK = -90 * b2.mV
-    ENa = 55 * b2.mV
-    gl = 0.1 * b2.msiemens
-    gK = 9 * b2.msiemens
-    gNa = 35 * b2.msiemens
-    C = 1 * b2.ufarad
-
-    tau_d = 9 * b2.ms
-    tau_r = 0.5 * b2.ms
+    neuron_e.vm_e = v0_e
+    neuron_e.m = "1 / (1 + betam/alpham)"
+    neuron_e.h = "1 / (1 + betah/alphah)"
+    neuron_e.n = "1 / (1 + betan/alphan)"
+    neuron_e.I_ext = I_ext_e
 
     # forming WB model with differential equations
-    eqs_wb = """
-    alphah = 0.35 * exp(-(vm + 58.0*mV) / (20.0*mV))/ms :Hz
-    alpham = 0.1/mV * (vm + 35.0*mV) / (1.0 - exp(-0.1/mV * (vm + 35.0*mV))) /ms :Hz
-    alphan = -0.05/mV * (vm + 34.0*mV) / (exp(-0.1/mV * (vm + 34.0*mV)) - 1.0)/ms :Hz
+    eqs_i = """
+    alphah = 0.35 * exp(-(vm_i + 58.0*mV) / (20.0*mV))/ms :Hz
+    alpham = 0.1/mV * (vm_i + 35.0*mV) / (1.0 - exp(-0.1/mV * (vm_i + 35.0*mV))) /ms :Hz
+    alphan = -0.05/mV * (vm_i + 34.0*mV) / (exp(-0.1/mV * (vm_i + 34.0*mV)) - 1.0)/ms :Hz
     
-    betah = 5.0 / (exp(-0.1/mV * (vm + 28.0*mV)) + 1.0)/ms :Hz
-    betam = 4.0 * exp(-(vm + 60.0*mV) / (18.0*mV))/ms :Hz
-    betan = 0.625 * exp(-(vm + 44.0*mV) / (80.0*mV))/ms :Hz
+    betah = 5.0 / (exp(-0.1/mV * (vm_i + 28.0*mV)) + 1.0)/ms :Hz
+    betam = 4.0 * exp(-(vm_i + 60.0*mV) / (18.0*mV))/ms :Hz
+    betan = 0.625 * exp(-(vm_i + 44.0*mV) / (80.0*mV))/ms :Hz
     
     m_inf = alpham / (alpham + betam) : 1
     
-    membrane_Im = I_ext + gNa*m_inf**3*h*(ENa-vm) + \
-        gl*(El-vm) + gK*n**4*(EK-vm) +gSyn*s_in*(ESyn-vm): amp
+    membrane_Im = I_ext + gNa*m_inf**3*h*(ENa-vm_i) + \
+        gl*(El-vm_i) + gK*n**4*(EK-vm_i) + I_syn_i: amp
     
     I_ext : amp
-    s_in  : 1
-    gSyn  : siemens
+    I_syn_i : amp
     
     dn/dt = alphan*(1-n)-betan*n : 1
     dh/dt = alphah*(1-h)-betah*h : 1
 
-    ds/dt = 0.5 * (1 + tanh(0.1*vm/mV)) * (1-s)/tau_r - s/tau_d : 1
+    ds_i/dt = 0.5 * (1 + tanh(0.1*vm_i/mV)) * (1-s_i)/tau_r_i - s_i/tau_d_i : 1
     
-    dvm/dt = membrane_Im/C : volt
+    dvm_i/dt = membrane_Im/C : volt
     """
-    neuron_wb = b2.NeuronGroup(num_wb, eqs_wb, method="euler", dt=0.01*b2.ms)
 
-    neuron_wb.vm = v0_wb
-    neuron_wb.h = "alphah / (alphah + betah)"
-    neuron_wb.n = "alphan / (alphan + betan)"
-    neuron_wb.I_ext = I_ext_wb
-    neuron_wb.s_in = 0
-    neuron_wb.gSyn = gSyn_wb
+    neuron_i = b2.NeuronGroup(num_i,
+                              eqs_i,
+                              method=integration_method,
+                              dt=0.01 * b2.ms,
+                              threshold='vm_i>-55*mV',
+                              refractory='vm_i>-55*mV',
+                              namespace=params_i)
 
-    S = b2.Synapses(neuron_rt,
-                    neuron_wb,
-                    '''
-                    w : 1
-                    s_in_post = w*s_pre:1 (summed)
-                    ''')
-    S.connect(i=0, j=0)
-    S.w[0, 0] = weight
+    neuron_i.vm_i = v0_i
+    neuron_i.h = "1 / (1 + betah / alphah)"
+    neuron_i.n = "1 / (1 + betan / alphan)"
+    neuron_i.I_ext = I_ext_i
+
+    #---------------------------------------------------------------#
+    syn_ei_eqs = '''
+    w : 1
+    I_syn_i_post = g_ei * s_e_pre * (v_rev_e - vm_i) :amp (summed)
+    '''
+    syn_ie_eqs = '''
+    w : 1
+    I_syn_e_post = g_ie * s_i_pre * (v_rev_i - vm_e) :amp (summed)
+    '''
+    #---------------------------------------------------------------#
+    S_ei = b2.Synapses(neuron_e,
+                       neuron_i,
+                       syn_ei_eqs,
+                       namespace=params_syn)
+    S_ei.connect(i=0, j=0)
+    S_ei.w = weight_ei
+    #---------------------------------------------------------------#
+    S_ie = b2.Synapses(neuron_i,
+                       neuron_e,
+                       syn_ie_eqs,
+                       namespace=params_syn)
+    S_ie.connect(i=0, j=0)
+    S_ie.w = weight_ie
 
     # tracking variables
-    st_mon_rt = b2.StateMonitor(neuron_rt, ["vm", "s_in"], record=True)
-    st_mon_wb = b2.StateMonitor(neuron_wb, ["vm", "s_in"], record=True)
+    st_mon_e = b2.StateMonitor(neuron_e, ["vm_e", "s_e"], record=True)
+    st_mon_i = b2.StateMonitor(neuron_i, ["vm_i", "s_i"], record=True)
 
     # running the simulation
-    net = b2.Network(neuron_rt)
-    net.add(neuron_wb)
-    net.add(st_mon_rt)
-    net.add(st_mon_wb)
-    net.add(S)
+    net = b2.Network(neuron_e)
+    net.add(neuron_i)
+    net.add(st_mon_e)
+    net.add(st_mon_i)
+    net.add(S_ei)
+    net.add(S_ie)
     net.run(simulation_time)
-    return st_mon_rt, st_mon_wb
+    return st_mon_e, st_mon_i
 
 
 if __name__ == "__main__":
 
-    st_mon_rt, st_mon_wb = simulate_2_cell_PING()
-    plot_data(st_mon_rt, st_mon_wb)
+    st_mon_e, st_mon_wb = simulate_2_cell_PING()
+    plot_data(st_mon_e, st_mon_wb)
