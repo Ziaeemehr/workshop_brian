dd19cd4e804bde975c15a23e031c3ec87c516800
Abolfazl Ziaeemehr <a.ziaeemehr@gmail.com>
2020-09-05 17:19:54 +0430

    efficiency added.

---
+++ efficiency/01_standalone.py
@@ -0,0 +1,16 @@
+import numpy as np
+import brian2 as b2
+import pylab as plt
+
+
+b2.start_scope()
+# b2.set_device('cpp_standalone', debug=True, directory="output")
+b2.prefs.codegen.target = 'numpy'  # use the Python fallback
+
+tau = 10*b2.ms
+eqs = '''
+dv/dt = (1-v)/tau : 1
+'''
+G = b2.NeuronGroup(1, eqs, method='exact')
+b2.run(100*b2.ms)
+print('After v = %s' % G.v[0])
--- efficiency/02_standalone.py
+++ efficiency/02_standalone.py
@@ -2,10 +2,7 @@ import numpy as np
 import brian2 as b2
 import pylab as plt
 
-
-b2.start_scope()
-b2.set_device('cpp_standalone', debug=True)
-# b2.prefs.codegen.target = 'numpy'  # use the Python fallback
+b2.set_device('cpp_standalone', build_on_run=False)
 
 tau = 10*b2.ms
 eqs = '''
@@ -13,4 +10,5 @@ dv/dt = (1-v)/tau : 1
 '''
 G = b2.NeuronGroup(1, eqs, method='exact')
 b2.run(100*b2.ms)
+b2.device.build(directory='output', compile=True, run=True, debug=False)
 print('After v = %s' % G.v[0])
